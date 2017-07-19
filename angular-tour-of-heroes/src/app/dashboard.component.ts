6978396f297d755f039892fbe56cbae028a5a83d
unmecenor <richard.guilloux@gmail.com>
2017-07-19 20:42:22 +0200

    [angular-tour-of-heroes] finish TUTORIAL -> 'ROUTING'

--- angular-tour-of-heroes/src/app/dashboard.component.ts
+++ angular-tour-of-heroes/src/app/dashboard.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { Hero } from "./hero";
+import { HeroService } from "./hero.service";
 
 @Component({
     selector: 'my-dashboard',
-    templateUrl: './dashboard.component.html'
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.css']
 })
-export class DashBoardComponent { };
+export class DashBoardComponent implements OnInit {
+    heroes: Hero[];
+    constructor(private heroService: HeroService) { }
+
+    ngOnInit(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes.slice(1, 5));
+    }
+};
--- angular-tour-of-heroes/src/app/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes.component.ts
@@ -1,7 +1,8 @@
-import { Component , OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from "./hero";
 import { HEROES } from "./heroes-mock";
 import { HeroService } from "./hero.service";
+import { Router } from "@angular/router";
 
 
 @Component({
@@ -9,12 +10,12 @@ import { HeroService } from "./hero.service";
   styleUrls: ['./heroes.component.css'],
   selector: 'my-heroes',
 })
-export class HeroesComponent implements OnInit{
+export class HeroesComponent implements OnInit {
   selectedHero: Hero;
   name = 'Angular';
   heroes: Hero[];
 
-  constructor(private heroService: HeroService){}
+  constructor(private router: Router, private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -24,8 +25,12 @@ export class HeroesComponent implements OnInit{
     this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
   }
 
-  onSelect(hero: Hero): void{
-    this.selectedHero=hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }
 
 }
